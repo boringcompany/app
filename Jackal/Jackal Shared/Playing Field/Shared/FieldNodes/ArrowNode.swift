8d3ecd8a970ed54a18f0964c827db57ee8230592
Andrey Zonov <azonov@dataart.com>
2018-02-28 10:39:01 +0300

    Add arrow Moves

--- Jackal/Jackal Shared/Playing Field/Shared/FieldNodes/ArrowNode.swift
+++ Jackal/Jackal Shared/Playing Field/Shared/FieldNodes/ArrowNode.swift
@@ -28,8 +28,45 @@ struct ArrowNode: FieldNodeDescribing {
     
     // MARK: Lifecycle
     init(rotation: Rotation, type: Type) {
-        let moves: [Move] = []
-        moveType = .oneOf(moves)
+        moveType = .oneOf(ArrowNode.moves(for: type))
         self.rotation = rotation
+    }
+    
+    // MARK: Private
+    private static func moves(for type: Type) -> [Move] {
+        let moves: [Move]
+        switch type {
+        case .unidirectionalStraight:
+            moves = [Move(x: 0, y: 1)]
+            
+        case .unidirectionalDiagonal:
+            moves = [Move(x: 1, y: 1)]
+            
+        case .bidirectionalStraight:
+            moves = [Move(x: 0, y: 1),
+                     Move(x: 0, y: -1)]
+            
+        case .bidirectionalDiagonal:
+            moves = [Move(x: 1, y: 1),
+                     Move(x: -1, y: -1)]
+            
+        case .threeDirectional:
+            moves = [Move(x: 0, y: 1),
+                     Move(x: 0, y: -1),
+                     Move(x: -1, y: 0)]
+            
+        case .fourDirectionalStraight:
+            moves = [Move(x: 0, y: 1),
+                     Move(x: 0, y: -1),
+                     Move(x: -1, y: 0),
+                     Move(x: 1, y: 0)]
+            
+        case .fourDirectionalDiagonal:
+            moves = [Move(x: -1, y: 1),
+                     Move(x: 1, y: -1),
+                     Move(x: -1, y: 1),
+                     Move(x: 1, y: -1)]
+        }
+        return moves
     }
 }
