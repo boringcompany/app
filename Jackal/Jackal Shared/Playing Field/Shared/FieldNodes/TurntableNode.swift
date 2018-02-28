58e03c52dd914d3d6800a46b65cd15aee84d7abb
Alexander Savonin <alexander.savonin@dataart.com>
2018-02-28 16:19:19 +0300

    Create TurnableNode

--- Jackal/Jackal Shared/Playing Field/Shared/FieldNodes/TurntableNode.swift
+++ Jackal/Jackal Shared/Playing Field/Shared/FieldNodes/TurntableNode.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2018 Boring Company. All rights reserved.
 //
 
-import Foundation
+struct TurntableNode: FieldNodeDescribing {
+    
+    // MARK: Public Data Structures
+    enum `Type`: String {
+        case twoSteps
+        case threeSteps
+        case fourSteps
+        case fiveSteps
+    }
+    
+    // MARK: Public Properties
+    var moveType: MoveType
+    var rotation: Rotation = .none
+    var canContainObject = true
+    var canStay = true
+    var actionType: ActionType = .permanent
+    var isOpen = false
+    var textureName: String
+    
+    // MARK: Lifecycle
+    init(type: Type) {
+        let moves: [Move] = [Move(x: -1, y: -1),
+                             Move(x:  0, y: -1),
+                             Move(x: +1, y: -1),
+                             Move(x: -1, y:  0),
+                             Move(x: +1, y:  0),
+                             Move(x: -1, y: +1),
+                             Move(x:  0, y: +1),
+                             Move(x: +1, y: +1)]
+        moveType = .oneOf(moves)
+        textureName = type.rawValue
+    }
+    
+    // MARK: Public
+    mutating func toggle() {
+        isOpen = !isOpen
+    }
+}
